763478038337bca2194ca1065f04d4e5f3e9e023
= <=>
2022-07-19 18:53:48 +0530

    added the filter task function

--- main.js
+++ main.js
@@ -49,6 +49,35 @@ function addTask(evt) {
 }
 
 function removeTask(evt) {
+    evt.preventDefault();
+    let taskName = '';
+    const elem = evt.target;
+    if (elem.classList.contains('delete-item')) {
+        taskName = elem.parentElement.textContent;
+        elem.parentElement.remove();
+    } else if (elem.parentElement.classList.contains('delete-item')) {
+        taskName = elem.parentElement.parentElement.textContent;
+        elem.parentElement.parentElement.remove();
+    }
+    if (taskName !== '') {
+        const indexOfDelete = taskName.lastIndexOf('delete');
+        taskName = taskName.substring(0, indexOfDelete);
 
+        tasks = tasks.filter(function(task) { return task !== taskName; });
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+}
 
+function clearAllTasks(evt) {
+    evt.preventDefault();
+    tasks = [];
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    // slower approach to remove all the child from the browsers memory!
+    // taskList.innerHTML = ''; here you are just making it empty space so there will be memeory
+
+    // faster approach to remove all the child from the browsers memory!
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
 }
